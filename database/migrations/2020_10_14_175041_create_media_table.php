48bb896fbd8f3d40b503333814409aa47066e8e2
Adelina <adelinacena123@gmail.com>
2020-10-14 21:44:10 +0200

    first commit

---
+++ database/migrations/2020_10_14_175041_create_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->text('url');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
